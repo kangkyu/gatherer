96327d3de192988e3b25a023b2ac6b120f95bed7
Kang-Kyu Lee <kangkyu1111@gmail.com>
2018-03-03 21:51:49 -0800

    Add project name presence validation

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,8 +6,12 @@ class ProjectsController < ApplicationController
 
   def create
     @action = CreatesProject.new(name: params[:project][:name], task_string: params[:project][:tasks])
-    @action.create
-    redirect_to projects_url
+    if @action.create
+      redirect_to projects_url
+    else
+      @project = @action.project
+      render :new
+    end
   end
 
   def index
