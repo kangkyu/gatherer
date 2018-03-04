66f707ce84c78f235c17aaa3decdfaead1a74fc8
Kang-Kyu Lee <kangkyu1111@gmail.com>
2018-03-03 21:34:57 -0800

    CreatesProject action object reads task string

--- app/actions/creates_project.rb
+++ app/actions/creates_project.rb
@@ -2,11 +2,25 @@ class CreatesProject
 
   attr_accessor :project
 
-  def initialize(name: "")
+  def initialize(name: "", task_string: "")
     @project_name = name
+    @task_string = task_string
   end
 
   def build
-    self.project = Project.create(name: @project_name)
+    self.project = Project.new(name: @project_name)
+    project.tasks = convert_string_to_tasks
+    project
+  end
+
+  def convert_string_to_tasks
+    @task_string.split("\n").map do |line|
+      title, size = line.split(":")
+      Task.create(title: title, size: (size.to_i.nonzero? || 1))
+    end
+  end
+
+  def create
+    build.save
   end
 end
